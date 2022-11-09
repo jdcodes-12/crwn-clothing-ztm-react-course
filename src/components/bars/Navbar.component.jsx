3d6509bbf66fd683678c81c1f3a460445c51c043
jdcodes-12 <johnnydicksoncode@gmail.com>
2022-11-09 17:24:45 -0600

    Finish Routing and style Navbar
    Setup routing to home and shop, styled Navbar, and added site logo to assets

--- src/components/bars/Navbar.component.jsx
+++ src/components/bars/Navbar.component.jsx
@@ -1,12 +1,17 @@
 import React from 'react';
 import { Outlet, Link} from 'react-router-dom';
+import { ReactComponent as CrwnLogo } from '../../assets/logos/crown.svg';
+
+import '../../styles/navbar.styles.scss';
 
 const Navbar = () => {
   return (
     <>
       <div className='navigation'>
         <Link to='/'>
-          <div className='logo-container'>Logo</div>
+          <div className='logo-container'>
+            <CrwnLogo />
+          </div>
         </Link>
         <div className='nav-links-container'>
           <Link className='nav-link' to='/shop'>Shop</Link>
