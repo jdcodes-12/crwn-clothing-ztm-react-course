47e4a356b5e88b8351cdb673c0934800489a6a08
jdcodes-12 <johnnydicksoncode@gmail.com>
2022-11-09 15:18:00 -0600

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
-function App() {
+const App = () =>  {
+
+  const collections = [
+    { id: 1, title: 'Hats', },
+    { id: 2, title: 'Jackets', },
+    { id: 3, title: 'Sneakers', },
+    { id: 4, title: 'Womens', },
+    { id: 5, title: 'Mens', },
+  ];
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="categories-container">
+      <div className="categories-container">
+       {collections.map(({id, title}) => (
+         <div className='categories-container'>
+          <div className="backgroun-image" />
+          <div id={id} className='category-body-container'>
+            <h2>{title}</h2>
+            <p>Shop Now</p>
+          </div>
+         </div>
+       ))}
+      </div>
     </div>
   );
 }
