0856443a824797c485deffb40068522a8a4c77a6
jdcodes-12 <johnnydicksoncode@gmail.com>
2022-11-09 15:58:15 -0600

    Create CategoryItem component
    Created the CategoryItem component to refactor code out of App.js. Additionally split up the SASS code

--- src/App.js
+++ src/App.js
@@ -1,26 +1,23 @@
 import React from 'react';
+import ComponentItem from './components/ComponentItem.component.jsx';
+
 import './styles/categories.styles.scss';
+
 
 const App = () =>  {
 
   const collections = [
-    { id: 1, title: 'Hats', },
-    { id: 2, title: 'Jackets', },
-    { id: 3, title: 'Sneakers', },
-    { id: 4, title: 'Womens', },
-    { id: 5, title: 'Mens', },
+    { id: 1, title: 'Hats',     imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',},
+    { id: 2, title: 'Jackets',  imageUrl: 'https://i.ibb.co/px2tCc3/jackets.png',},
+    { id: 3, title: 'Sneakers', imageUrl: 'https://i.ibb.co/0jqHpnp/sneakers.png',},
+    { id: 4, title: 'Womens',   imageUrl: 'https://i.ibb.co/GCCdy8t/womens.png',},
+    { id: 5, title: 'Mens',     imageUrl: 'https://i.ibb.co/R70vBrQ/men.png'},
   ];
 
   return (
     <div className='categories-container'>
-       {collections.map(({id, title}) => (
-         <div key={id} className='category-container'>
-          <div className='background-image' />
-          <div id={id} className='category-body-container'>
-            <h2>{title}</h2>
-            <p>Shop Now</p>
-          </div>
-         </div>
+       {collections.map(({ id, title, imageUrl }) => (
+        <ComponentItem id={id} title={title} imageUrl={imageUrl}/>
        ))}
     </div>
   );
